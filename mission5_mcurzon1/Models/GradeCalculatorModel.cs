f683fc6ec5c11e9f7667c41997dba2d472ee68d4
mattcurzon <mcurzon1@byu.edu>
2023-02-06 12:58:26 -0700

    Add project files.

---
+++ mission5_mcurzon1/Models/GradeCalculatorModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mission5_mcurzon1.Models
+{
+    public class GradeCalculatorModel
+    {
+        // make models required and inside the range of 0-100
+        [Required]
+        [Range(0,100)]
+        public float Assignment { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public float Project { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public float Intex { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public float Final { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public float Quiz { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public float Midterm { get; set; }
+
+    }
+}
---
+++ mission5_mcurzon1/wwwroot/js/GradeCalculator.js
@@ -0,0 +1,45 @@
+﻿$("#submit").click(function () {
+    // set variable values to input values on html
+    var assignment = $("#assignment").val()
+    var project = $("#project").val()
+    var midterm = $("#midterm").val()
+    var quiz = $("#quiz").val()
+    var final = $("#final").val()
+    var intex = $("#intex").val()
+
+    // calculate the values based on the chart
+    var finalGrade = (assignment * 0.5) + (project * 0.1) + (midterm * 0.1) + (quiz * 0.1) + (final * 0.1) + (intex * 0.1);
+
+    // find letter grade from final grade percentage
+    if (finalGrade >= 94) {
+        var letterGrade = 'A'
+    }
+    else if (finalGrade >= 90) {
+        var letterGrade = 'A-'
+    } else if (finalGrade >= 87) {
+        letterGrade = 'B+'
+    } else if (finalGrade >= 84) {
+        letterGrade = 'B'
+    } else if (finalGrade >= 80) {
+        letterGrade = 'B-'
+    } else if (finalGrade >= 77) {
+        letterGrade = 'C+'
+    } else if (finalGrade >= 74) {
+        letterGrade = 'C'
+    } else if (finalGrade >= 70) {
+        letterGrade = 'C-'
+    } else if (finalGrade >= 67) {
+        letterGrade = 'D+'
+    } else if (finalGrade >= 64) {
+        letterGrade = 'D'
+    } else if (finalGrade >= 60) {
+        letterGrade >= 'D-'
+    } else {
+        letterGrade = 'E'
+    }
+
+    //print final letter grade in html and alert
+    $("#finalGrade").html("Final Grade: " + letterGrade)
+
+    alert("Your final grade is: " + letterGrade)
+})
